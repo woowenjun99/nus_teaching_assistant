c43d23e24f6c55b4eb14fcf98bba914f19cf748b
woowenjun99 <woowenjun99@gmail.com>
2024-02-01 16:24:09 +0800

    Update to use transaction

--- src/server/db/migration/000001_init_migration.down.sql
+++ src/server/db/migration/000001_init_migration.down.sql
@@ -1,2 +1,4 @@
-DROP TABLE IF EXISTS Users, Tutorials, TutorialMembers, TutorialQuestions CASCADE;
-DROP TYPE IF EXISTS Roles;
+BEGIN;
+    DROP TABLE IF EXISTS Users, Tutorials, TutorialMembers, TutorialQuestions CASCADE;
+    DROP TYPE IF EXISTS Roles;
+COMMIT;
--- src/server/db/migration/000001_init_migration.up.sql
+++ src/server/db/migration/000001_init_migration.up.sql
@@ -1,39 +1,41 @@
-CREATE TABLE Users (
-	created_at TIMESTAMPTZ(6) DEFAULT(CURRENT_TIMESTAMP),
-	is_admin BOOL NOT NULL DEFAULT(FALSE),
-	email TEXT UNIQUE NOT NULL,
-	full_name TEXT,
-	student_id TEXT PRIMARY KEY
-);
+BEGIN;
+	CREATE TABLE Users (
+		created_at TIMESTAMPTZ(6) DEFAULT(CURRENT_TIMESTAMP),
+		is_admin BOOL NOT NULL DEFAULT(FALSE),
+		email TEXT UNIQUE NOT NULL,
+		full_name TEXT,
+		student_id TEXT PRIMARY KEY
+	);
 
-CREATE TABLE Tutorials (
-	course_code TEXT NOT NULL,
-	course_offering TEXT NOT NULL,
-	is_course_over BOOL DEFAULT(FALSE),
-	joined_at TIMESTAMPTZ(6) DEFAULT(CURRENT_TIMESTAMP),
-	PRIMARY KEY (course_code, course_offering)
-);
+	CREATE TABLE Tutorials (
+		course_code TEXT NOT NULL,
+		course_offering TEXT NOT NULL,
+		is_course_over BOOL DEFAULT(FALSE),
+		joined_at TIMESTAMPTZ(6) DEFAULT(CURRENT_TIMESTAMP),
+		PRIMARY KEY (course_code, course_offering)
+	);
 
-CREATE TYPE Roles AS ENUM('students', 'teaching_assistant');
+	CREATE TYPE Roles AS ENUM('students', 'teaching_assistant');
 
-CREATE TABLE TutorialMembers (
-	course_code TEXT NOT NULL,
-	course_offering TEXT NOT NULL,
-	student_id TEXT NOT NULL,
-	student_role Roles NOT NULL DEFAULT('students'),
-	FOREIGN KEY (student_id) REFERENCES Users ON DELETE CASCADE,
-	FOREIGN KEY (course_code,
-		course_offering) REFERENCES Tutorials (course_code,
-		course_offering),
-	PRIMARY KEY (student_id, course_code, course_offering)
-);
+	CREATE TABLE TutorialMembers (
+		course_code TEXT NOT NULL,
+		course_offering TEXT NOT NULL,
+		student_id TEXT NOT NULL,
+		student_role Roles NOT NULL DEFAULT('students'),
+		FOREIGN KEY (student_id) REFERENCES Users ON DELETE CASCADE,
+		FOREIGN KEY (course_code,
+			course_offering) REFERENCES Tutorials (course_code,
+			course_offering),
+		PRIMARY KEY (student_id, course_code, course_offering)
+	);
 
-CREATE TABLE TutorialQuestions (
-	created_at TIMESTAMPTZ(6) NOT NULL DEFAULT(CURRENT_TIMESTAMP),
-	course_code TEXT NOT NULL,
-	course_offering TEXT NOT NULL,
-	question TEXT NOT NULL,
-	question_id UUID PRIMARY KEY DEFAULT(gen_random_uuid()),
-	student_id TEXT NOT NULL,
-	FOREIGN KEY (student_id, course_code, course_offering) REFERENCES TutorialMembers ON DELETE CASCADE
-);
+	CREATE TABLE TutorialQuestions (
+		created_at TIMESTAMPTZ(6) NOT NULL DEFAULT(CURRENT_TIMESTAMP),
+		course_code TEXT NOT NULL,
+		course_offering TEXT NOT NULL,
+		question TEXT NOT NULL,
+		question_id UUID PRIMARY KEY DEFAULT(gen_random_uuid()),
+		student_id TEXT NOT NULL,
+		FOREIGN KEY (student_id, course_code, course_offering) REFERENCES TutorialMembers ON DELETE CASCADE
+	);
+COMMIT;
