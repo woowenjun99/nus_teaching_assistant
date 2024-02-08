922d404661d08990dabd31399d019c9a8fae9f2f
woowenjun99 <woowenjun99@gmail.com>
2024-02-08 20:12:28 +0800

    Remove unused dependency

--- src/server/api/procedures/admin/getAllTutorials.ts
+++
@@ -1,7 +0,0 @@
-import { getAllTutorialGroups } from "~/server/db/sqlc/tutorial_groups_sql";
-import { protectedProcedure } from "../../trpc";
-import { pool } from "~/server/db/pool";
-
-export const getAllTutorialsProcedure = protectedProcedure.query(() =>
-  getAllTutorialGroups(pool),
-);
--- src/server/api/routers/tutorialGroups.ts
+++ src/server/api/routers/tutorialGroups.ts
@@ -1,12 +1,10 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { getUserTutorialsProcedure } from "../procedures/tutorials/getUserTutorials";
-import { getAllTutorialsProcedure } from "../procedures/admin/getAllTutorials";
+import { getUserTutorialsProcedure } from "../procedures/users/getUserTutorials";
 import { createTutorialGroupProcedure } from "../procedures/admin/createTutorialGroup";
 import { deleteTutorialGroupProcedure } from "../procedures/admin/deleteTutorialGroup";
 
 export const tutorialGroupsRouter = createTRPCRouter({
   createTutorialGroup: createTutorialGroupProcedure,
   deleteTutorialGroup: deleteTutorialGroupProcedure,
-  getAllTutorials: getAllTutorialsProcedure,
   getUserTutorials: getUserTutorialsProcedure,
 });
